b2ef76de30201c4aa55debb343afabbf23307268
isnoto <noto.rsv@gmail.com>
2016-01-11 16:33:06 +0200

    Fix travis

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -14,6 +14,7 @@ describe 'Projects', js: true do
         click_link 'Projects'
         fill_in :project_name, with: 'Test'
         click_button 'Add project!'
+        puts page.body
         expect(page).to have_content 'Project Test was successfully created'
         expect(page).to have_link 'Show tasks'
       end
